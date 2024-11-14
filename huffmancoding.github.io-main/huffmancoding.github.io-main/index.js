f872aac0fbe94d48855fc7bdbc6062c3dae99ffa
zohaib <zohaibkhanzohaibi2003@gmail.com>
2024-11-14 15:22:38 +0500

    setting tw hogyi hy integration rehti

--- huffmancoding.github.io-main/huffmancoding.github.io-main/index.js
+++ huffmancoding.github.io-main/huffmancoding.github.io-main/index.js
@@ -2,16 +2,44 @@ document.getElementById('text-encoding-button').addEventListener('click', functi
     document.getElementById('text-layout').style.display = 'block';
     document.getElementById('file-layout').style.display = 'none';
     document.getElementById('image-layout').style.display = 'none';
+    document.getElementById('nav-layout').style.display = 'none';
+});
+
+document.getElementById('text-encode-button').addEventListener('click', function() {
+    document.getElementById('encoded-text').style.display = 'block';    
+    document.getElementById('encoded-codes').style.display = 'block';    
+
 });
 
 document.getElementById('file-upload-button').addEventListener('click', function() {
     document.getElementById('text-layout').style.display = 'none';
     document.getElementById('file-layout').style.display = 'block';
     document.getElementById('image-layout').style.display = 'none';
+    document.getElementById('nav-layout').style.display = 'none';
+    document.getElementById('file-decode-button').style.display = 'block';
 });
 
 document.getElementById('image-compression-button').addEventListener('click', function() {
     document.getElementById('text-layout').style.display = 'none';
     document.getElementById('file-layout').style.display = 'none';
     document.getElementById('image-layout').style.display = 'block';
+    document.getElementById('nav-layout').style.display = 'none';
+});
+
+document.getElementById('file-encode-button').addEventListener('click', function() {
+    document.getElementById('download-encoded-file').style.display = 'block';
+    document.getElementById('compression-ratio').style.display = 'block';
+    document.getElementById('download-decoded-file').style.display = 'none';
+});
+
+document.getElementById('file-decode-button').addEventListener('click', function() {
+    ocument.getElementById('download-encoded-file').style.display = 'none';
+    document.getElementById('compression-ratio').style.display = 'none';
+    document.getElementById('download-decoded-file').style.display = 'block';
+});
+
+document.getElementById('image-encode-button').addEventListener('click', function() {
+    document.getElementById('download-encoded-image').style.display = 'block';
+    document.getElementById('image-compression-ratio').style.display = 'block';
+    
 });
